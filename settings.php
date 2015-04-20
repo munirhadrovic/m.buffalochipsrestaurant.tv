9269d6b816a740759c2decac3b85241b18ff59f0
novicaa <novicamiketic@gmail.com>
2015-04-20 14:10:17 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,13 @@
+
+<?php
+$adresa[0]="26620 Old 41 Road,Bonita Springs, FL 34135"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="FL 34135";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Bonita Springs";
+$telephone[0]=2399471000;// Broj koji se poziva na dugme CALL US
+
+
+$companynumber=1;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
